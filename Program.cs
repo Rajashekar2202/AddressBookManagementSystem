44583a9e630d31af815bdbee1d5172593543cb36
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-11-06 11:45:30 +0530

    added create contacts program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AddressBookManagement
+{
+    class Program
+    {
+        public static List<CreateContacts> contactList = new List<CreateContacts>();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBookManagmentSystem");
+
+            CreateContacts contact1 = new CreateContacts();
+            Console.WriteLine("Enter first name: ");
+            contact1.firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter last name: ");
+            contact1.lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter address: ");
+            contact1.address = Console.ReadLine();
+
+            Console.WriteLine("Enter city: ");
+            contact1.city = Console.ReadLine();
+
+            Console.WriteLine("Enter state: ");
+            contact1.state = Console.ReadLine();
+
+            Console.WriteLine("Enter phone: ");
+            contact1.phone = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter email: ");
+            contact1.email = Console.ReadLine();
+
+            Console.WriteLine("Enter zipcode: ");
+            contact1.zipcode = Convert.ToInt32(Console.ReadLine());
+
+
+            contactList.Add(contact1);
+        }
+    }
+}
